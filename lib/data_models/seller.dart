7b9a384e12fc81b2d29af4a12d4cf939d71e3993
ghackg11 <56071613+ghackg11@users.noreply.github.com>
2020-11-20 01:05:43 +0530

    Update seller.dart

--- lib/data_models/seller.dart
+++ lib/data_models/seller.dart
@@ -1,19 +1,31 @@
+
+import 'package:flutter/material.dart';
+import 'package:main_app/screens/sell_page_part2.dart';
+
 class Seller {
-  final int id; //  (Seller ID) Generated upon addition
-  final String name;
-  final int contact;
+  final String name = '';
+  final int contact ;
+  final int id;
+  final String shopName = '';
+  final String shopAddress = '';
   final double latitude;
   final double longitude;
-  final String shopName;
-  final String shopAddress;
+  static int count=0;
 
-  const Seller(
-    this.id,
-    this.name,
-    this.contact,
-    this.latitude,
-    this.longitude,
-    this.shopName,
-    this.shopAddress,
-  );
+  Seller(
+      {this.name,
+      this.contact,
+      this.shopName,
+      this.shopAddress,
+      this.id,
+      this.latitude,
+      this.longitude}){
+    count++;
+    this.id = count;
+  }
+
+  save() {
+    print('Seller Saved');
+  }
 }
+
